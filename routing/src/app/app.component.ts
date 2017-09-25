4c81d9ed869b8790739d8ad261e40a27c6dbb43d
karanpatel <karan.patel.sa@gmail.com>
2017-09-25 12:31:07 +0530

    built in pipes

--- routing/src/app/app.component.ts
+++ routing/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = 'karanpatel';
+  day = new Date(1995, 24, 9);
 }
